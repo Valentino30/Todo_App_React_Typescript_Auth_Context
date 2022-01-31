2feed7c412271c4eac8b5c62c1c7a95a9a09e1c9
Valentino <valentino@secret-source.eu>
2022-01-31 15:37:08 +0100

    Remove axios

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,12 +1,7 @@
-// import axios from "axios";
 import { nanoid } from "nanoid";
-
-// const baseUrl = process.env.REACT_APP_API;
 
 export const apiCall = {
   post: async (endpoint: string, body: {}) => {
-    // const targetUrl = baseUrl + endpoint;
-    // const response = await axios.post(targetUrl, body);
     const response = {
       data: {
         id: nanoid(),
